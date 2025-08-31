cea2b4cf7134e00e3bae0dd25dee56fd42cda62b
JAutoDetailing <andres131711@outlook.com>
2025-08-31 08:21:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple test to make sure JS works
+console.log("Website loaded!");
+
+document.querySelector("h1").addEventListener("click", () => {
+  alert("Welcome to Shine Pro Detailing!");
+});
